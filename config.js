145c272ef68d8eb69b79c410dfe1efb6bb27afa1
hnt1306ks <108657043+hnt1306ks@users.noreply.github.com>
2022-10-20 14:44:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'to: Cà Chua';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100072798814092';
+const eventName = 'From: Anh zai nào đó';
+const titleCard = '20/10 zui zẻ nhó';
+const contentCard = 'Chúc em mau ăn chóng lớn và ngày càng xinh đẹp nhé :))';
+const giftImage = 'mthu2.png';
+//const base64 = '';
+//const giftImageBase64 = "data:image/png;base64, " + base64;
